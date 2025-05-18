d168f2d20ce99ad99dc7c28a4e2e362e9670b8c7
DarkSwarm123 <minecraftmodfandarkswarm@gmail.com>
2025-05-18 20:55:04 +0200

    Update Pet Simulator 99.lua

--- Pet Simulator 99.lua
+++ Pet Simulator 99.lua
@@ -279,7 +279,7 @@ local DaycareToggle = MainTab:CreateToggle({
                     task.wait(1)
                     local args = {
                         [1] = {
-                            ["6d9ae73648cf4160a5d8ee216929b5e5"] = 54
+                            ["6d9ae73648cf4160a5d8ee216929b5e5"] = 55
                         }
                     }
                     game:GetService("ReplicatedStorage").Network:FindFirstChild("Daycare: Enroll"):InvokeServer(unpack(args))
