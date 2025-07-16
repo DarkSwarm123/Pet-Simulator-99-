19e93f7bbbb135c377b3538ac1f146c01d8761f2
DarkSwarm123 <minecraftmodfandarkswarm@gmail.com>
2025-07-16 11:00:46 +0200

    Update Pet Simulator 99.lua

--- Pet Simulator 99.lua
+++ Pet Simulator 99.lua
@@ -47,7 +47,7 @@ orb.CollectDistance = math.huge
 orb.CombineDistance = math.huge
 orb.CombineDelay = 0
 orb.SoundDistance = 0
-orb.BillboardDistance = math.huge
+orb.BillboardDistance = 0
 
 local function Wait(x)
     local startTick = tick()  -- Czas rzeczywisty
